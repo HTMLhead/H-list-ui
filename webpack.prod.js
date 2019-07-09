aebe6720a9513aef6099390a749b52654d00cd42
HTMLhead <mrdoo309@gmail.com>
2019-07-09 23:20:23 +0900

    update(webpack): 중복 처리

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,90 +1,33 @@
-const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserJSPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-module.exports = [{
-  mode: 'production',
-  entry: {
-    "listUi.min": [path.resolve(__dirname, 'src/js/listUi.js'), "./src/assets/sass/main.scss"]
-  },
-  // 빌드한 파일이 저장되는 곳
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    publicPath: '/',
-    libraryTarget: "umd",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/, // 모듈폴더제외
-        use: {
-          loader: 'babel-loader',
-        }
-      },
-      {
-        test: /\.(sass|scss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ]
-      }
-    ]
-  },
-  plugins: [
-    // MiniCssExtractPlugin 플러그인 설정
-    // [name]은 entry에서 설정한 "listUi"를 의미
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    })
-  ],
-  optimization: {
-    minimizer: [
-      new TerserJSPlugin({}),
-      new OptimizeCSSAssetsPlugin({}),
-    ]
-  }
-},{
-  mode: 'production',
-  entry: {
-    "listUi": [path.resolve(__dirname, 'src/js/listUi.js'),"./src/assets/sass/main.scss"],
-  },
-  // 빌드한 파일이 저장되는 곳
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    publicPath: '/',
-    libraryTarget: "umd",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/, // 모듈폴더제외
-        use: {
-          loader: 'babel-loader',
-        }
-      },
-      {
-        test: /\.(sass|scss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ]
-      }
-    ]
-  },
-  plugins: [
-    // MiniCssExtractPlugin 플러그인 설정
-    // [name]은 entry에서 설정한 "listUi"를 의미
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    })
-  ],
-  optimization: {
-    minimize: false,
-  }
-}]
+const path = require("path");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = [
+  merge(common, {
+    entry: {
+      listUi: [
+        path.resolve(__dirname, "src/js/listUi.js"),
+        "./src/assets/sass/main.scss",
+      ],
+    },
+    // 웹팩 빌드를 시작할 때 dist폴더를 비우도록 설정
+    plugins: [new CleanWebpackPlugin()],
+    optimization: {
+      minimize: false,
+    },
+  }),
+  merge(common, {
+    entry: {
+      "listUi.min": [
+        path.resolve(__dirname, "src/js/listUi.js"),
+        "./src/assets/sass/main.scss",
+      ],
+    },
+    optimization: {
+      minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+  }),
+];
