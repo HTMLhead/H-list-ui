faaad3654b2d4884f1644fc768bc90d6c35f1176
HTMLhead <mrdoo309@gmail.com>
2019-07-11 22:52:29 +0900

    feat(main): 컴포넌트 사용가이드 제작

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,22 @@
-import { CarouselListUi } from "./listUis/carouselListUi.js";
+import { listUi } from "./listUis/listUi.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const carouselListUi = new CarouselListUi({
-    targetElement: "h-list",
-    content: [
-      {
-        link: "https://www.naver.com",
-        title: "네이버",
-        description: "초록색의 대표",
-        thumbnail: "https://sungmoon.files.wordpress.com/2013/04/naver.jpg",
-      },
-    ],
+  listUi.createList({
+    type: "typeA",
+    containerEl: ".h-list",
+    data: {
+      title: ["naver", "codesquad", "mrdoo309"],
+      description: ["best Of green", "title of code academy", "dude"],
+      link: ["https://www.nave.com", "https://codesquad.kr", "https://medium.com"],
+      thumbnail: [
+        "https://yt3.ggpht.com/a/AGF-l7-5-hIaLpaYcO4KP1B30jcepRKfwrwli2suxA=s288-mo-c-c0xffffffff-rj-k-no",
+        "https://codesquad.kr/img/company/codesquad2.png",
+        "https://miro.medium.com/max/940/1*3BP9i12zmh99F4fyjUdi3w.png",
+      ],
+    },
     style: {
-      width: "850px",
-      height: "100px",
-      transition: true,
+      width: "600",
+      height: "200",
     },
   });
 });
