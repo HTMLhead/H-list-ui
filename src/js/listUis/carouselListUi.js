3aa7a164cb266f0c743b890971e9b7c9c29ca412
HTMLhead <mrdoo309@gmail.com>
2019-07-11 22:53:19 +0900

    update(listUi): 리스트 UI화면에 띄우는 기능 제작

--- src/js/listUis/carouselListUi.js
+++ src/js/listUis/carouselListUi.js
@@ -1,12 +1,25 @@
 class CarouselListUi {
-  constructor({ targetElement, content, style }) {
-    Object.assign(this, { targetElement, content, style });
-    this.init();
+  constructor({ containerEl, data, style }) {
+    console.log(containerEl);
+    Object.assign(this, { containerEl, data, style });
   }
 
-  init() {
-    this.container = document.querySelector(`.${this.targetElement}`);
-    this.container.innerHTML = `<div class="something">hihihi<div>`;
+  render() {
+    this.container = document.querySelector(this.containerEl);
+    this.addContentList();
+  }
+
+  addContentList() {
+    this.data.title.forEach((v, i) => {
+      this.container.innerHTML += `
+      <a href="${this.data.link[i]}"
+        <div class="h-list${i}">
+          <img src="${this.data.thumbnail[i]}"
+          <div class="h-list-title${i}>${this.data.title}</div>
+          <div class="h-list-desc${i}>${this.data.description[i]}</div>
+        <div>
+      </a>`;
+    });
   }
 }
 
