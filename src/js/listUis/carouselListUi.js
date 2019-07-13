efb9f540d555afd9ee40a377ab8285e0d6ca5a6e
HTMLhead <mrdoo309@gmail.com>
2019-07-13 22:47:44 +0900

    update(carouselListUi): container스타일 변경 및 ContentList의 테스트가 용이하도록 수정

--- src/js/listUis/carouselListUi.js
+++ src/js/listUis/carouselListUi.js
@@ -1,25 +1,30 @@
 class CarouselListUi {
   constructor({ containerEl, data, style }) {
-    console.log(containerEl);
     Object.assign(this, { containerEl, data, style });
   }
 
   render() {
     this.container = document.querySelector(this.containerEl);
-    this.addContentList();
+    this.container.style.width = `${this.style.width}px`;
+    this.container.style.height = `${this.style.height}px`;
+    this.container.style.overflow = `hidden`;
+    this.container.innerHTML += this.addContentList();
   }
 
   addContentList() {
+    let contentDom = "";
     this.data.title.forEach((v, i) => {
-      this.container.innerHTML += `
-      <a href="${this.data.link[i]}"
-        <div class="h-list${i}">
-          <img src="${this.data.thumbnail[i]}"
-          <div class="h-list-title${i}>${this.data.title}</div>
-          <div class="h-list-desc${i}>${this.data.description[i]}</div>
-        <div>
+      contentDom += `
+      <a href="${this.data.link[i]}">
+        <div class="h-list">
+          <img class="h-list-image" src="${this.data.thumbnail[i]}">
+          <div class="h-list-title">${this.data.title[i]}</div>
+          <div class="h-list-desc">${this.data.description[i]}</div>
+        </div>
       </a>`;
     });
+
+    return contentDom;
   }
 }
 
