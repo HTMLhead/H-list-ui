d6fd6f7d5f4255260eab19f720e03a0f62af578d
HTMLhead <mrdoo309@gmail.com>
2019-07-15 22:41:32 +0900

    update(main): 필요없는 DomElement값 제거

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,35 @@
 import { listUi } from "./listUis/listUi.js";
 
+// [
+//   "https://yt3.ggpht.com/a/AGF-l7-5-hIaLpaYcO4KP1B30jcepRKfwrwli2suxA=s288-mo-c-c0xffffffff-rj-k-no",
+//   "https://codesquad.kr/img/company/codesquad2.png",
+//   "https://miro.medium.com/max/940/1*3BP9i12zmh99F4fyjUdi3w.png",
+// ]
+
+const titleArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
+const thumbnailArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
+const linkArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
+const descArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
+
 document.addEventListener("DOMContentLoaded", () => {
   listUi.createList({
     type: "typeA",
-    containerEl: ".h-list-container",
+    domElement: ".h-list-container",
     data: {
-      title: ["naver", "codesquad", "mrdoo309"],
-      description: ["best Of green", "title of code academy", "dude"],
-      link: ["https://www.nave.com", "https://codesquad.kr", "https://medium.com"],
-      thumbnail: [
-        "https://yt3.ggpht.com/a/AGF-l7-5-hIaLpaYcO4KP1B30jcepRKfwrwli2suxA=s288-mo-c-c0xffffffff-rj-k-no",
-        "https://codesquad.kr/img/company/codesquad2.png",
-        "https://miro.medium.com/max/940/1*3BP9i12zmh99F4fyjUdi3w.png",
-      ],
+      titleArr: titleArray,
+      descriptionArr: descArray,
+      linkArr: linkArray,
+      thumbnailArr: thumbnailArray,
     },
     style: {
-      width: "800",
-      height: "300",
+      container: {
+        width: "800",
+        height: "300",
+      },
+      content: {
+        width: "200",
+        height: "300",
+      },
     },
   });
 });
