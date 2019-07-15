3367d21d82d7a66733027df5fdbae55f6d6f5191
HTMLhead <mrdoo309@gmail.com>
2019-07-15 22:37:57 +0900

    update(carouselListUi): keydown이벤트 생성, layout, selector엘리먼트 생성 및 스타일 적용 render메서드 제작

--- src/js/listUis/carouselListUi.js
+++ src/js/listUis/carouselListUi.js
@@ -8,11 +8,115 @@ class CarouselListUi {
   }
 
   render() {
-    this.container = document.querySelector(this.containerEl);
-    this.container.style.width = `${this.style.width}px`;
-    this.container.style.height = `${this.style.height}px`;
-    this.container.style.overflow = `hidden`;
-    this.container.innerHTML += this.addContentList();
+    const container = document.querySelector(this.domElement);
+    container.appendChild(this.addLayout());
+    container.appendChild(this.addSelector());
+    const layout = document.querySelector(".h-list-layout");
+    this.addContainerStyle(container);
+    this.addLayoutStyle(layout);
+    this.addLayoutEvent(layout);
+    layout.innerHTML += this.addContentList();
+  }
+
+  addLayout() {
+    const selector = document.createElement("div");
+    selector.className = "h-list-layout";
+    selector.tabIndex = 0;
+    return selector;
+  }
+
+  addSelector() {
+    const selector = document.createElement("div");
+    selector.style.width = `${Number(this.style.content.width) + 20}px`;
+    selector.style.height = `${Number(this.style.content.height) + 20}px`;
+    selector.style.position = `absolute`;
+    selector.style.top = `0`;
+    selector.style.backgroundColor = `#008000`;
+    selector.style.opacity = `0.5`;
+    selector.className = "h-list-selector";
+    return selector;
+  }
+
+  moveSelector(transformSize) {
+    const selector = document.querySelector(`.h-list-selector`);
+    this.link = this.data.linkArr[this.contentIndex + this.overContentIndex];
+    selector.style.transform = `translateX(${transformSize}px)`;
+    return selector.style.transform;
+  }
+
+  goLink() {
+    window.location.href = this.link;
+  }
+
+  addContainerStyle(element) {
+    element.style.width = `${this.style.container.width}px`;
+    element.style.height = `${this.style.container.height}px`;
+    element.style.position = `relative`;
+    element.style.overflow = `hidden`;
+    return element;
+  }
+
+  addLayoutStyle(element) {
+    element.style.display = `flex`;
+    element.style.outline = `none`;
+    element.style.width = `${this.wholeWidth}px`;
+    return element;
+  }
+
+  addLayoutEvent(element) {
+    const contentWidth = this.style.content.width;
+    const containerWidth = this.style.container.width;
+    const wholeWidth = this.wholeWidth;
+    element.addEventListener("keydown", e =>
+      this.moveElement(e, element, contentWidth, containerWidth, wholeWidth)
+    );
+  }
+
+  moveElement(e, element, contentWidth, containerWidth, wholeWidth) {
+    if (e.key === "ArrowRight") {
+      this.changeLayoutRight(element, contentWidth, containerWidth, wholeWidth);
+    } else if (e.key === "ArrowLeft") {
+      this.changeLayoutLeft(element, contentWidth, containerWidth, wholeWidth);
+    } else if (e.key === "Enter") {
+      this.goLink();
+    }
+  }
+
+  changeLayoutLeft(element, contentWidth, containerWidth, wholeWidth) {
+    const originSize = Number(element.style.transform.replace(/[^0-9-]/g, ""));
+    const contentWidthSize = Number(contentWidth);
+    if (originSize >= 0) {
+      return;
+    }
+    if (Math.abs(originSize) > wholeWidth - containerWidth && this.overContentIndex > 0) {
+      this.overContentIndex--;
+      this.moveSelector((contentWidthSize + 20) * this.overContentIndex);
+      return;
+    }
+    this.contentIndex--;
+    this.link = this.data.linkArr[this.contentIndex];
+    element.style.transform = `translateX(${originSize + contentWidthSize + 20}px)`;
+  }
+
+  changeLayoutRight(element, contentWidth, containerWidth, wholeWidth) {
+    const originSize = Number(element.style.transform.replace(/[^0-9-]/g, ""));
+    const contentWidthSize = Number(contentWidth);
+    if (Math.abs(originSize) > wholeWidth - containerWidth) {
+      this.selectorMoveChecker(contentWidthSize, containerWidth);
+      return;
+    }
+    this.contentIndex++;
+    this.link = this.data.linkArr[this.contentIndex];
+    element.style.transform = `translateX(${originSize - contentWidthSize - 20}px)`;
+  }
+
+  selectorMoveChecker(contentWidth, containerWidth) {
+    if ((contentWidth + 20) * (this.overContentIndex + 1) > containerWidth - contentWidth) {
+      return;
+    }
+    this.overContentIndex++;
+    this.moveSelector((contentWidth + 20) * this.overContentIndex);
+    return;
   }
 
   addContentList() {
