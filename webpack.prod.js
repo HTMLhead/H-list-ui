385e4a5981dd2b61d51c4f387ac64d12690591ca
HTMLhead <mrdoo309@gmail.com>
2019-07-28 01:02:32 +0900

    update(webpack.prod): js파일만 번들링 하도록 수정

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,12 +8,9 @@ const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 module.exports = [
   merge(common, {
     entry: {
-      listUi: [
-        path.resolve(__dirname, "src/js/listUis/carouselListUi.js"),
-        "./src/assets/sass/main.scss",
-      ],
+      listUi: path.resolve(__dirname, "src/js/listUis/listUi.js"),
     },
-    // 웹팩 빌드를 시작할 때 dist폴더를 비우도록 설정
+    // 웹팩 빌드를 시작할 때 dist폴더를 비우도록 설정i
     plugins: [new CleanWebpackPlugin()],
     optimization: {
       minimize: false,
@@ -21,10 +18,7 @@ module.exports = [
   }),
   merge(common, {
     entry: {
-      "listUi.min": [
-        path.resolve(__dirname, "src/js/listUis/carouselListUi.js"),
-        "./src/assets/sass/main.scss",
-      ],
+      "listUi.min": path.resolve(__dirname, "src/js/listUis/listUi.js"),
     },
     optimization: {
       minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
