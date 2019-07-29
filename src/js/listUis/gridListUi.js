7f808c16e45bd2bed4a6b0c33070a8c4883671d3
HTMLhead <mrdoo309@gmail.com>
2019-07-30 00:50:44 +0900

    update(gridListUi): container의 width를 정하는 부분 수정

--- src/js/listUis/gridListUi.js
+++ src/js/listUis/gridListUi.js
@@ -36,7 +36,7 @@ class GridListUi {
       v.style.fontWeight = this.style.title.bold ? "bold" : "null";
     });
     desc.forEach(v => {
-      v.style.fontSize = this.style.description;
+      v.style.fontSize = this.style.description.fontSize;
     });
   }
 
@@ -123,8 +123,11 @@ class GridListUi {
   addContainerStyle(element) {
     element.tabIndex = 2;
     element.style.position = "relative";
-    element.style.width = `${this.style.container.width}px`;
+    element.style.width = `${Number(this.style.container.width) + 10}px`;
     element.style.height = `${this.style.container.height}px`;
+    element.style.border = this.style.border
+      ? `${this.style.border.size}px solid ${this.style.border.color}`
+      : null;
     element.style.overflow = "hidden";
     // container element에 Grid 속성을 추가 및 반응형 CSS style추가
   }
